1387a58e3fc4f61e29fc0a0e9488a06250a70a92
Mindy Jen <jencmhep@gmail.com>
2021-02-19 14:26:43 -0600

    update_210219

---
+++ MindyTwitterAnalysis/src/main/scala/com/cjen/tweetsapp/json2csv.scala
@@ -0,0 +1,42 @@
+package tweetsapp
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.types.StringType
+
+object json2csv {
+    
+    def run(spark: SparkSession): Unit = {
+        import spark.implicits._
+
+        //val stringify = udf((vs: Seq[String]) => vs match {
+        //    case null => null
+        //    case _    => s"""[${vs.mkString(",")}]"""
+        //})
+
+        val df = spark.read.json("ana2_BTS_geoOnly_210216_morning_CDT_json/ana2_BTS_geoOnly_210216_morning_CDT.json")
+        .withColumn("index", $"index".cast(IntegerType))
+        .withColumn("Hr", $"Hr".cast(IntegerType))
+        .withColumn("Min", $"Min".cast(IntegerType))
+        .withColumn("Sec", $"Sec".cast(IntegerType))
+        .withColumn("Retweets", $"Retweets".cast(IntegerType))
+        .withColumn("Replies", $"Replies".cast(IntegerType))
+        .withColumn("Likes", $"Likes".cast(IntegerType))
+        .withColumn("Quotes", $"Quotes".cast(IntegerType))
+        .withColumn("Place", $"Place".cast(StringType))
+
+        df.show()
+
+        df.printSchema()   
+
+        val dumpCSV = df.write.csv("ana2_BTS_geoOnly_210216_morning_CDT_csv")
+
+    }
+
+    //def stringify(c: Column) = concat(lit("["), concat_ws(",", c), lit("]")) 
+    //https://stackoverflow.com/questions/40426106/spark-2-0-x-dump-a-csv-file-from-a-dataframe-containing-one-array-of-type-string
+
+}
+
