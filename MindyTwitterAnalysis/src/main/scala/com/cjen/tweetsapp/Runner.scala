c1e5152290291e5c85384212703268a55491e035
Mindy Jen <jencmhep@gmail.com>
2021-02-20 11:41:15 -0600

    update_210220

--- MindyTwitterAnalysis/src/main/scala/com/cjen/tweetsapp/Runner.scala
+++ MindyTwitterAnalysis/src/main/scala/com/cjen/tweetsapp/Runner.scala
@@ -21,68 +21,29 @@ import scala.concurrent.Future
 object Runner {
     
     def main(args: Array[String]): Unit = {
-      
-    val spark = SparkSession
+
+        if(args.length != 3) {
+            println("Options:")
+            println("tweetSparkSql [input file] [output folder]")
+            println("tweetMoreSparkSql [input folder/input file] [output folder]")
+            println("json2csv [input folder/input file] [output folder]")
+            System.exit(1)
+        }
+
+        val spark = SparkSession
         .builder()
         .appName("Hello Spark SQL")
         .master("local[4]")
         .getOrCreate()
       
-    import spark.implicits._
-
-    spark.sparkContext.setLogLevel("WARN")
-
-    //tweetSparkSql(spark)
-
-    //tweetMoreSparkSql.run(spark)
-
-    json2csv.run(spark)
-
-    }
-
-    def tweetSparkSql(spark: SparkSession): Unit = {
-
         import spark.implicits._
 
-        val df = spark.read.option("multiline", "true").json("BTS_geoOnly_210216_5am-6am_CDT.json")
+        spark.sparkContext.setLogLevel("WARN")
 
-        df.show()
-
-        df.printSchema()
-
-        df.select(countDistinct("data.id")).show()
-
-        df.select(countDistinct("data.created_at")).show()
-
-        df.select(countDistinct("includes.places.full_name")).show()
-
-        df.groupBy("includes.places.full_name").count().show() 
-        
-        val windowGeo  = Window.partitionBy("includes.places.full_name").orderBy("data.created_at")
-        df.withColumn("row_number", row_number.over(windowGeo)).show()
-
-        val dfgeo = df.groupBy($"includes.places.full_name")
-        .agg(
-            functions.count($"includes.places.full_name").as("counts"),
-            functions.avg($"data.public_metrics.retweet_count").as("avg_Retweet"),
-            functions.avg($"data.public_metrics.reply_count").as("avg_Replies"),
-            functions.avg($"data.public_metrics.like_count").as("avg_Like"),
-            functions.avg($"data.public_metrics.quote_count").as("avg_Quotes")
-        )
-        .sort($"counts".desc)
-
-        dfgeo.show(1000, false)
-
-        dfgeo.explain(true)
-
-        println(dfgeo.rdd.toDebugString)
-
-        val w = Window.orderBy($"counts".desc)
-        val result = dfgeo.withColumn("index", row_number().over(w))
-
-        result.show(1000, false)
-        result.repartition(1).write.format("json").save("ana_BTS_geoOnly_210216_morning_CDT_json")
-
+        if(args(0) == "tweetSparkSql") tweetSparkSql.run(spark, args(1), args(2))
+        else if(args(0) == "tweetMoreSparkSql") tweetMoreSparkSql.run(spark, args(1), args(2))
+        else if(args(0) == "json2csv") json2csv.run(spark, args(1), args(2))
+    
     }
 
 }
